39c40282c0453734ec5133712a8c471d4af8474b
Michael Kocher <mkocher@pacificbiosciences.com>
2015-07-02 13:36:37 -0800

    [Bug 26333 and  26080]. Minor changes to notebook utils.

--- notebooks/launch.sh
+++ notebooks/launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 set +x
 use_port=7777
 echo "Running on port ${use_port}"
-ipython notebook --certfile=/home/UNIXHOME/mkocher/mycert.pem --profile=nbserver --pylab inline --port=$use_port --no-browser --log-level 50
+ipython notebook --certfile=/home/UNIXHOME/mkocher/mycert.pem --profile=nbserver  --port=$use_port --no-browser --log-level 50
--- notebooks/reinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip uninstall -y pbsmrtpipe
-pip install ../
