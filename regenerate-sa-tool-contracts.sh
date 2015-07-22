34113690510493bac13bbadeb4e1cc3bd39d6bc3
Michael Kocher <mkocher@pacificbiosciences.com>
2015-07-22 11:44:38 -0800

    [Bug 26987 and 26333]. For clarity of dependencies, split the SA3 tool contract generation to it's own script.

---
+++ regenerate-sa-tool-contracts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+TC_DIR=./pbsmrtpipe/pb_static_tasks/
+
+# For now, all the SA tools that can emit a TC can be done here.
+python -m pbreports.report.sat --emit-tool-contract > $TC_DIR/pbreports_report_sat_tool_contract.json
+python -m pbreports.report.mapping_stats --emit-tool-contract > $TC_DIR/mapping_stats_tool_contract.json
+python -m pbreports.report.variants --emit-tool-contract > $TC_DIR/pbreports_report_variants_tool_contract.json
+
+ipdSummary.py --emit-tool-contract > $TC_DIR/kinetics_tools_ipdsummary_tool_contract.json
+variantCaller --emit-tool-contract > $TC_DIR/genomic_consensus_variantcaller_tool_contract.json
+motifMaker_find.py --emit-tool-contract > $TC_DIR/motif_maker_find_tool_contract.json
+motifMaker_reprocess.py --emit-tool-contract > $TC_DIR/motif_maker_reprocess_tool_contract.json
+
--- regenerate-tool-contracts.sh
+++ regenerate-tool-contracts.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash +x
 
+# This is for pbsmrtpipe (and subdependencies) ONLY. DO NOT put random
+# SA# tools in here. They should NOT be assumed to be installed.
+
 TC_DIR=./pbsmrtpipe/pb_static_tasks/
 
 python -m pbsmrtpipe.tools_dev.fasta --emit-tool-contract > $TC_DIR/dev_tools_fasta_tool_contract.json
@@ -9,12 +12,3 @@ python -m pbsmrtpipe.tools_dev.fasta_report --emit-tool-contract > $TC_DIR/dev_f
 # version changes.
 python -m pbcommand.cli.examples.dev_app --emit-tool-contract > $TC_DIR/dev_pbcommand_examples_dev_app_tool_contract.json
 python -m pbcommand.cli.examples.dev_txt_app --emit-tool-contract > $TC_DIR/dev_pbcommand_examples_dev_txt_app_tool_contract.json
-
-python -m pbreports.report.sat --emit-tool-contract > $TC_DIR/pbreports_report_sat_tool_contract.json
-python -m pbreports.report.mapping_stats --emit-tool-contract > $TC_DIR/mapping_stats_tool_contract.json
-python -m pbreports.report.variants --emit-tool-contract > $TC_DIR/pbreports_report_variants_tool_contract.json
-
-ipdSummary.py --emit-tool-contract > $TC_DIR/kinetics_tools_ipdsummary_tool_contract.json
-variantCaller --emit-tool-contract > $TC_DIR/genomic_consensus_variantcaller_tool_contract.json
-motifMaker_find.py --emit-tool-contract > $TC_DIR/motif_maker_find_tool_contract.json
-motifMaker_reprocess.py --emit-tool-contract > $TC_DIR/motif_maker_reprocess_tool_contract.json
