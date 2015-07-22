bd626a6a29159273e2aa91588f187261a4f456ac
Nathaniel Echols <nechols@pacb.com>
2015-07-22 06:27:51 -0800

    new tool contracts; bug 26968, bug 27108

--- regenerate-tool-contracts.sh
+++ regenerate-tool-contracts.sh
@@ -9,3 +9,10 @@ python -m pbsmrtpipe.tools_dev.fasta_report --emit-tool-contract > $TC_DIR/dev_f
 # version changes.
 python -m pbcommand.cli.examples.dev_app --emit-tool-contract > $TC_DIR/dev_pbcommand_examples_dev_app_tool_contract.json
 python -m pbcommand.cli.examples.dev_txt_app --emit-tool-contract > $TC_DIR/dev_pbcommand_examples_dev_txt_app_tool_contract.json
+
+python -m pbreports.report.sat --emit-tool-contract > $TC_DIR/pbreports_report_sat_tool_contract.json
+python -m pbreports.report.mapping_stats --emit-tool-contract > $TC_DIR/mapping_stats_tool_contract.json
+
+ipdSummary.py --emit-tool-contract > $TC_DIR/kinetics_tools_ipdsummary_tool_contract.json
+variantCaller --emit-tool-contract > $TC_DIR/genomic_consensus_variantcaller_tool_contract.json
+motifMaker.py --emit-tool-contract > $TC_DIR/motif_maker_tool_contract.json
