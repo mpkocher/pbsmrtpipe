2a3c75a75d9d5527778eb4a9f0a85dd1bc1fcef9
Nathaniel Echols <nechols@pacb.com>
2015-07-23 10:09:02 -0800

    TCI support for top_variants report; bug 26968

--- regenerate-sa-tool-contracts.sh
+++ regenerate-sa-tool-contracts.sh
@@ -7,6 +7,7 @@ TC_DIR=./pbsmrtpipe/pb_static_tasks/
 python -m pbreports.report.sat --emit-tool-contract > $TC_DIR/pbreports_report_sat_tool_contract.json
 python -m pbreports.report.mapping_stats --emit-tool-contract > $TC_DIR/mapping_stats_tool_contract.json
 python -m pbreports.report.variants --emit-tool-contract > $TC_DIR/pbreports_report_variants_tool_contract.json
+python -m pbreports.report.top_variants --emit-tool-contract > $TC_DIR/pbreports_report_top_variants_tool_contract.json
 
 pbalign --emit-tool-contract > $TC_DIR/pbalign_tool_contract.json
 ipdSummary.py --emit-tool-contract > $TC_DIR/kinetics_tools_ipdsummary_tool_contract.json
