35fe956d0d0fbeae96d18955de3140caa3123fba
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-01-25 16:08:40 -0800

    Drop internal tag

--- pbsmrtpipe/pb_pipelines/pb_pipelines_falcon.py
+++ pbsmrtpipe/pb_pipelines/pb_pipelines_falcon.py
@@ -181,7 +181,7 @@ def hgap_cmd():
     logging_cfg = '$entry:e_02'
     return _get_hgap_pypeflow(hgap_cfg, logging_cfg, subreadset)
 
-@dev_register("hgap_lean", "Assembly (HGAP 5) without reports", tags=("internal",))
+@dev_register("hgap_lean", "Assembly (HGAP 5) without reports")
 def hgap_lean():
     """GUI polished HGAP pipeline (sans reports).
     (TODO: Add hgap_fat for reports.)
