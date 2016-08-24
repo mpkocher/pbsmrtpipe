a9b8045fb1252469c8c59587537e2f10da1675d0
yech <yech1990@gmail.com>
2016-08-24 13:20:18 +0800

    edit launch script of jupyter notebook
    fix a typo and make it compatible to run on remote server.

--- notebooks/launch.sh
+++ notebooks/launch.sh
@@ -4,4 +4,4 @@ set +x
 use_port=7777
 echo "Running on port ${use_port}"
 #ipython notebook --certfile=/home/UNIXHOME/mkocher/mycert.pem --profile=nbserver  --port=$use_port --no-browser --log-level 50
-jupyter notebook --port=${use_port} --no-browser --log-level 50
+jupyter notebook --port=${use_port} --no-browser --log-level=50 --ip="*"
