9e8fd7940d48950ebc0c58ce15a84d3231f055fe
natechols <nathaniel.echols@gmail.com>
2017-01-06 12:07:41 -0800

    task/pipeline for testing task option types

--- pbsmrtpipe/pb_pipelines/pb_pipelines_dev.py
+++ pbsmrtpipe/pb_pipelines/pb_pipelines_dev.py
@@ -177,3 +177,15 @@ def get_reference_ds_report():
     bs = list(itertools.chain(*[to_b(x) for x in xrange(max_txt_tasks)]))
 
     return b1 + b2 + bs
+
+
+MISC_TASK_OPTIONS = {
+    "pbcommand.task_options.alpha": 50,
+    "pbcommand.task_options.ploidy": "diploid",
+    "pbcommand.task_options.epsilon": 0.01
+}
+@dev_register("dev_task_options", "Dev Task Option Support Pipeline", tags=("local", ), task_options=MISC_TASK_OPTIONS)
+def get_dev_task_options_pipeline():
+    """Simple example pipeline"""
+    b1 = [('$entry:e_01', 'pbsmrtpipe.tasks.dev_mixed_app:0')]
+    return b1
