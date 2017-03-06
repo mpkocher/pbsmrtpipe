b189b685fd439ecb81935a024cfdb1426d6965a3
natechols <nathaniel.echols@gmail.com>
2017-03-06 11:24:57 -0800

    force h5py reinstall

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -8,6 +8,7 @@ mkdir -p tmp
 python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
 source tmp/venv/bin/activate
 
+pip install --no-cache-dir h5py
 (cd repos/pbcommand && make install)
 pip install -r REQUIREMENTS_CI.txt
 pip install -r REQUIREMENTS.txt
