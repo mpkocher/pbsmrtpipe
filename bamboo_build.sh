7b85a1f73f699419f62ebf98221cb456ca88becf
natechols <nathaniel.echols@gmail.com>
2017-03-06 09:29:56 -0800

    use central hdf5 library

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+source /mnt/software/Modules/current/init/bash
+module load hdf5-tools/1.8.16
+
 mkdir -p tmp
 /opt/python-2.7.9/bin/python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
 source tmp/venv/bin/activate
