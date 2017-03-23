f1207a54d644d19503cece3d9740ffcbc3ce894d
Ben Lerch <blerch@smtp.nanofluidics.com>
2017-03-23 10:07:15 -0700

    minor variants report pipeline defintion bugfix

--- pbsmrtpipe/pb_pipelines/pb_pipelines_sa3.py
+++ pbsmrtpipe/pb_pipelines/pb_pipelines_sa3.py
@@ -808,7 +808,7 @@ def _core_minorseq_multiplexed(ds_ccs, ds_ref):
         (ds_ccs, "pysiv2.tasks.minor_variants:2")
     ]
     report = [
-        ("pysiv2.tasks.minor_variants:1", "pbreports.tasks.minor_variants_report:0")
+        ("pysiv2.tasks.minor_variants:0", "pbreports.tasks.minor_variants_report:0")
     ]
     return align + julietflow + report
 
