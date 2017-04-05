b9ee8199fae78c10bac46670ff8fa7fd932ffa26
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-04-05 11:01:42 -0700

    no fabric needed

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -20,9 +20,7 @@ $PIP install --user \
   $NX3PBASEURL/pythonpkgs/avro-1.7.7-cp27-none-any.whl \
   $NX3PBASEURL/pythonpkgs/Jinja2-2.7.3-cp27-none-any.whl \
   $NX3PBASEURL/pythonpkgs/pyparsing-1.5.7-cp27-none-any.whl \
-  $NX3PBASEURL/pythonpkgs/pydot-1.0.28-cp27-none-any.whl \
-  $NX3PBASEURL/pythonpkgs/pycrypto-2.6.1-cp27-cp27mu-linux_x86_64.whl \
-  $NX3PBASEURL/pythonpkgs/Fabric-1.10.1-cp27-none-any.whl
+  $NX3PBASEURL/pythonpkgs/pydot-1.0.28-cp27-none-any.whl
 $PIP install --user -e repos/pbcommand
 $PIP install --user -e repos/pbcore
 $PIP install --user -e repos/pbcoretools
