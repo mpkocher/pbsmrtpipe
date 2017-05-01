1513feca79f18eab754ff8eb96b732e766a6aa7c
natechols <nathaniel.echols@gmail.com>
2017-05-01 13:35:54 -0700

    s/BETA/Beta/, plus new tag

--- pbsmrtpipe/pb_pipelines/pb_pipelines_sa3.py
+++ pbsmrtpipe/pb_pipelines/pb_pipelines_sa3.py
@@ -77,6 +77,8 @@ class Tags(object):
     INTERNAL = "internal"
     # Mulit-analysis jobs
     COND = "conditions"
+    # Beta pipelines
+    BETA = "beta"
 
     RESEQ = (MAP, CONSENSUS)
     RESEQ_INTERNAL = (MAP, CONSENSUS, INTERNAL)
@@ -839,12 +841,12 @@ MV_CCS_OPTS = {
     "pbccs.task_options.rich_qvs": True
 }
 
-@sa3_register("sa3_ds_minorseq", "Minor Variants Analysis [BETA]", "0.1.0", tags=(Tags.MINORVAR,), task_options=MV_CCS_OPTS)
+@sa3_register("sa3_ds_minorseq", "Minor Variants Analysis [Beta]", "0.1.0", tags=(Tags.MINORVAR,Tags.BETA), task_options=MV_CCS_OPTS)
 def ds_minorseq():
     return _core_ccs(Constants.ENTRY_DS_SUBREAD) + _core_minorseq_multiplexed("pbccs.tasks.ccs:0", Constants.ENTRY_DS_REF)
 
 
-@sa3_register("sa3_ds_barcode_minorseq", "Minor Variants Analysis with Barcoding [BETA]", "0.1.0", tags=(Tags.MINORVAR,Tags.BARCODE), task_options=MV_CCS_OPTS)
+@sa3_register("sa3_ds_barcode_minorseq", "Minor Variants Analysis with Barcoding [Beta]", "0.1.0", tags=(Tags.MINORVAR,Tags.BARCODE,Tags.BETA), task_options=MV_CCS_OPTS)
 def ds_barcode_minorseq():
     b1 = _core_barcode()
     subreadset = "pbcoretools.tasks.bam2bam_barcode:0"
