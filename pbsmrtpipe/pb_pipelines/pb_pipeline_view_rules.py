2566f943479d2f5a84a0967ba545650b2d966e7e
natechols <nathaniel.echols@gmail.com>
2017-08-03 10:16:54 -0700

    default blacklisting for any file that isn't explicitly visible, plus cleanup to distinguish whitelists and blacklists

--- pbsmrtpipe/pb_pipelines/pb_pipeline_view_rules.py
+++ pbsmrtpipe/pb_pipelines/pb_pipeline_view_rules.py
@@ -14,26 +14,90 @@ import sys
 from pbcommand.models import (FileTypes, DataStoreViewRule,
                               PipelineDataStoreViewRules)
 
+from pbsmrtpipe.loader import load_all
+
+
 log = logging.getLogger(__name__)
 
 REGISTERED_VIEW_RULES = {}
+RTASKS, _, _, PIPELINES = load_all()
 
 
 def _to_view_rule(args):
     return DataStoreViewRule(*args)
 
 
+def __to_view_rule_triples(rules, is_hidden):
+    assert all([len(rule)==2 for rule in rules])
+    return [(rule[0], rule[1], is_hidden) for rule in rules]
+
+
+def _to_whitelist(rules):
+    """
+    Shorthand for whitelisting files given just the source IDs and file types.
+    Not suitable for cases where we need to override the label or description.
+    """
+    return __to_view_rule_triples(rules, is_hidden=False)
+
+
+def _to_blacklist(rules):
+    """
+    Shorthand for blacklisting files given just the source IDs and file types.
+    Not suitable for cases where we need to override the label or description.
+    """
+    return __to_view_rule_triples(rules, is_hidden=True)
+
+
 def load_pipeline_view_rules(registered_views_d, pipeline_id, smrtlink_version,
                              view_rules):
+    """
+    Generate a complete set of view rules for output files in the specified
+    pipeline.  This will use the built-in pipeline templates and registered
+    tool contracts to automatically populate the rules with default
+    blacklisting for any file that isn't explicitly whitelisted in the input
+    view_rules list.
+
+    :param registered_views_d: dictionary of pipeline datastore view rules,
+                               keyed by pipeline id
+    :param pipeline_id: string id, e.g. 'pbsmrtpipe.pipelines.sa3_sat'
+    :param smrtlink_version: release version associated with the rule
+    :param view_rules: list of explicit rules as tuples of at least 3 elements
+    """
+    pipeline_id_ = "pbsmrtpipe.pipelines.{p}".format(
+        p=pipeline_id.split(".")[-1])
+    view_rules_ = [_to_view_rule(r) for r in view_rules]
+    explicit_rules = {r.source_id:r for r in view_rules_}
+    pipeline = PIPELINES[pipeline_id_]
+    for (bound_input, bound_output) in pipeline.all_bindings:
+        out_ids = bound_output.split(":")
+        task_id = out_ids[0]
+        for i_file, file_type in enumerate(RTASKS[task_id].output_types):
+            source_id = "{t}-out-{i}".format(t=task_id, i=i_file)
+            if source_id in explicit_rules:
+                rule_file_type_id = explicit_rules[source_id].file_type_id
+                if rule_file_type_id != file_type.file_type_id:
+                    log.warn("File type mismatch for %s: %s vs. %s",
+                             source_id, rule_file_type_id,
+                             file_type.file_type_id)
+            else:
+                log.info("Using default rule for %s in %s", source_id,
+                         pipeline_id_)
+                rule = DataStoreViewRule(source_id, file_type.file_type_id,
+                                         True)
+                view_rules_.append(rule)
     pvr = PipelineDataStoreViewRules(
-        pipeline_id="pbsmrtpipe.pipelines.{p}".format(p=pipeline_id),
+        pipeline_id=pipeline_id_,
         smrtlink_version=smrtlink_version,
-        rules=[_to_view_rule(r) for r in view_rules])
-    registered_views_d[pipeline_id] = pvr
+        rules=view_rules_)
+    registered_views_d[pipeline_id_] = pvr
     return registered_views_d
 
 
 def register_pipeline_rules(pipeline_id, smrtlink_version):
+    """
+    Decorator function which processes view rules for storage in the global
+    dict.
+    """
     def deco_wrapper(func):
         if pipeline_id in REGISTERED_VIEW_RULES:
             log.warn("'{i}' already has view rules defined".format(
@@ -45,6 +109,21 @@ def register_pipeline_rules(pipeline_id, smrtlink_version):
         def wrapper(*args, **kwds):
             return func(*args, **kwds)
     return deco_wrapper
+
+
+def set_default_view_rules():
+    """
+    Any pipeline that is visible to customers but doesn't have view rules set
+    here gets the default blacklist.  (In practice, we probably shouldn't let
+    this happen.)
+    """
+    for pipeline_id, pipeline in PIPELINES.iteritems():
+        if not "dev" in pipeline.tags and not "internal" in pipeline.tags:
+            if not pipeline_id in REGISTERED_VIEW_RULES:
+                log.warn("%s does not have view rules, using defaults",
+                         pipeline_id)
+                load_pipeline_view_rules(REGISTERED_VIEW_RULES, pipeline_id,
+                                         "5.0", [])
 
 
 def _mapping_report_rules():
@@ -140,88 +219,95 @@ def basemod_and_motif_view_rules():
 
 @register_pipeline_rules("polished_falcon_fat", "3.2")
 def hgap4_view_rules():
-    return [
-        ("pbcoretools.tasks.bam2fasta-out-0", FileTypes.FASTA , True),
-        ("pbcoretools.tasks.fasta2fofn-out-0", FileTypes.FOFN , True),
+    whitelist = _to_whitelist([
+        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-1", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-2", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon0_merge-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon0_cons-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-1", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-2", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon1_merge-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon1_db2falcon-out-0", FileTypes.TXT)
+    ])
+    blacklist = _to_blacklist([
+        ("pbcoretools.tasks.bam2fasta-out-0", FileTypes.FASTA),
+        ("pbcoretools.tasks.fasta2fofn-out-0", FileTypes.FOFN),
+        ("falcon_ns.tasks.task_falcon_make_fofn_abs-out-0", FileTypes.FOFN),
+        ("falcon_ns.tasks.task_falcon0_build_rdb-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon0_build_rdb-out-1", FileTypes.TXT),
+        ("genomic_consensus.tasks.variantcaller-out-0", FileTypes.GFF),
+        ("genomic_consensus.tasks.variantcaller-out-1", FileTypes.VCF),
+        ("genomic_consensus.tasks.gff2bed-out-0", FileTypes.BED),
+        ("falcon_ns.tasks.task_falcon1_build_pdb-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon0_run_daligner_jobs-out-0", FileTypes.FOFN),
+        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-0", FileTypes.FOFN),
+        ("falcon_ns.tasks.task_falcon1_run_daligner_jobs-out-0", FileTypes.FOFN),
+        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-0", FileTypes.FOFN),
+        ("falcon_ns.tasks.task_falcon2_run_asm-out-0", FileTypes.FASTA),
+        ("falcon_ns.tasks.task_falcon_gen_config-out-0", FileTypes.CFG),
+        ("falcon_ns.tasks.task_falcon0_build_rdb-out-2", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon_config-out-0", FileTypes.JSON),
+        ("pbreports.tasks.polished_assembly-out-0", FileTypes.REPORT),
+        ("falcon_ns.tasks.task_report_preassembly_yield-out-0", FileTypes.REPORT),
+        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-2", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-1", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-1", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-2", FileTypes.JSON),
+        ("falcon_ns.tasks.task_falcon0_merge-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon0_cons-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon1_merge-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon1_db2falcon-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon0_rm_las-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon1_rm_las-out-0", FileTypes.TXT),
+        ("falcon_ns.tasks.task_falcon2_rm_las-out-0", FileTypes.TXT)
+    ])
+    return blacklist + whitelist + [
         ("pbcoretools.tasks.contigset2fasta-out-0", FileTypes.FASTA, False, "Polished Assembly"),
-        ("falcon_ns.tasks.task_falcon_make_fofn_abs-out-0", FileTypes.FOFN , True),
-        ("falcon_ns.tasks.task_falcon0_build_rdb-out-0", FileTypes.TXT , True),
-        ("falcon_ns.tasks.task_falcon0_build_rdb-out-1", FileTypes.TXT,True),
-        ("genomic_consensus.tasks.variantcaller-out-0", FileTypes.GFF,True),
-        ("genomic_consensus.tasks.variantcaller-out-1", FileTypes.VCF,True),
         ("genomic_consensus.tasks.variantcaller-out-2", FileTypes.DS_CONTIG, False, "Polished Assembly"),
         ("genomic_consensus.tasks.variantcaller-out-3", FileTypes.FASTQ, False, "Polished Assembly"),
-        ("genomic_consensus.tasks.gff2bed-out-0", FileTypes.BED,True),
-        ("falcon_ns.tasks.task_falcon1_build_pdb-out-0", FileTypes.TXT , True),
-        ("falcon_ns.tasks.task_falcon0_run_daligner_jobs-out-0", FileTypes.FOFN , True),
-        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-0", FileTypes.FOFN , True),
-        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-1", FileTypes.JSON , False),
-        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-2", FileTypes.JSON , False),
-        ("falcon_ns.tasks.task_falcon0_merge-out-0", FileTypes.TXT , False),
-        ("falcon_ns.tasks.task_falcon0_cons-out-0", FileTypes.TXT , False),
-        ("falcon_ns.tasks.task_falcon1_run_daligner_jobs-out-0", FileTypes.FOFN , True),
-        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-0", FileTypes.FOFN , True),
-        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-1", FileTypes.JSON , False),
-        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-2", FileTypes.JSON , False),
-        ("falcon_ns.tasks.task_falcon1_merge-out-0", FileTypes.TXT , False),
-        ("falcon_ns.tasks.task_falcon1_db2falcon-out-0", FileTypes.TXT , False),
-        ("falcon_ns.tasks.task_falcon2_run_asm-out-0", FileTypes.FASTA, True),
-        ("falcon_ns.tasks.task_falcon_gen_config-out-0", FileTypes.CFG , True),
-        ("falcon_ns.tasks.task_falcon0_build_rdb-out-2", FileTypes.TXT , True),
-        ("falcon_ns.tasks.task_falcon_config-out-0", FileTypes.JSON, True),
-        ("pbreports.tasks.polished_assembly-out-0", FileTypes.REPORT , True),
-        ("falcon_ns.tasks.task_report_preassembly_yield-out-0", FileTypes.REPORT , True),
-        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-2", FileTypes.JSON, True),
-        ("falcon_ns.tasks.task_falcon0_run_merge_consensus_jobs-out-1", FileTypes.JSON, True),
-        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-1", FileTypes.JSON, True),
-        ("falcon_ns.tasks.task_falcon1_run_merge_consensus_jobs-out-2", FileTypes.JSON, True),
-        ("falcon_ns.tasks.task_falcon0_merge-out-0", FileTypes.TXT, True),
-        ("falcon_ns.tasks.task_falcon0_cons-out-0", FileTypes.TXT, True),
-        ("falcon_ns.tasks.task_falcon1_merge-out-0", FileTypes.TXT, True),
-        ("falcon_ns.tasks.task_falcon1_db2falcon-out-0", FileTypes.TXT, True),
-        ("falcon_ns.tasks.task_falcon0_rm_las-out-0", FileTypes.TXT, True),
-        ("falcon_ns.tasks.task_falcon1_rm_las-out-0", FileTypes.TXT, True),
-        ("falcon_ns.tasks.task_falcon2_rm_las-out-0", FileTypes.TXT, True),
         ("pbcoretools.tasks.fasta2referenceset-out-0", FileTypes.DS_REF, False, "Draft Assembly")
     ] + _mapping_report_rules()
 
 
 @register_pipeline_rules("hgap_fat", "3.2")
 def hgap5_view_rules():
-    return [
-        ("falcon_ns.tasks.task_hgap_prepare-out-0", FileTypes.JSON, True),
-        ("falcon_ns.tasks.task_hgap_prepare-out-1", FileTypes.JSON, True),
-        ("falcon_ns.tasks.task_hgap_prepare-out-2", FileTypes.LOG, True),
-        ("falcon_ns.tasks.task_hgap_run-out-1", FileTypes.REPORT, True),
-        ("falcon_ns.tasks.task_hgap_run-out-2", FileTypes.REPORT, True),
-        ("falcon_ns.tasks.task_hgap_run-out-3", FileTypes.LOG, True)
-    ]
+    return _to_blacklist([
+        ("falcon_ns.tasks.task_hgap_prepare-out-0", FileTypes.JSON),
+        ("falcon_ns.tasks.task_hgap_prepare-out-1", FileTypes.JSON),
+        ("falcon_ns.tasks.task_hgap_prepare-out-2", FileTypes.LOG),
+        ("falcon_ns.tasks.task_hgap_run-out-1", FileTypes.REPORT),
+        ("falcon_ns.tasks.task_hgap_run-out-2", FileTypes.REPORT),
+        ("falcon_ns.tasks.task_hgap_run-out-3", FileTypes.LOG)
+    ])
 
 
 def _isoseq_view_rules():
-    return _isoseq_report_rules() + _ccs_report_rules() + [
-        ("pbtranscript.tasks.separate_flnc-out-0", FileTypes.PICKLE, True),
-        ("pbtranscript.tasks.create_chunks-out-0", FileTypes.PICKLE, True),
-        ("pbtranscript.tasks.create_chunks-out-1", FileTypes.PICKLE, True),
-        ("pbtranscript.tasks.create_chunks-out-2", FileTypes.PICKLE, True),
-        ("pbtranscript.tasks.combine_cluster_bins-out-0", FileTypes.FASTA, False),
-        ("pbtranscript.tasks.combine_cluster_bins-out-1", FileTypes.JSON, True),
-        ("pbtranscript.tasks.combine_cluster_bins-out-2", FileTypes.CSV, False),
-        ("pbtranscript.tasks.combine_cluster_bins-out-3", FileTypes.DS_CONTIG, False),
-        ("pbtranscript.tasks.combine_cluster_bins-out-4", FileTypes.FASTQ, False),
-        ("pbtranscript.tasks.combine_cluster_bins-out-5", FileTypes.DS_CONTIG, False),
-        ("pbtranscript.tasks.combine_cluster_bins-out-6", FileTypes.FASTQ, False),
-        ("pbtranscript.tasks.combine_cluster_bins-out-7", FileTypes.PICKLE, True),
+    whitelist = _to_whitelist([
+        ("pbtranscript.tasks.combine_cluster_bins-out-0", FileTypes.FASTA),
+        ("pbtranscript.tasks.combine_cluster_bins-out-2", FileTypes.CSV),
+        ("pbtranscript.tasks.combine_cluster_bins-out-3", FileTypes.DS_CONTIG),
+        ("pbtranscript.tasks.combine_cluster_bins-out-4", FileTypes.FASTQ),
+        ("pbtranscript.tasks.combine_cluster_bins-out-5", FileTypes.DS_CONTIG),
+        ("pbtranscript.tasks.combine_cluster_bins-out-6", FileTypes.FASTQ),
+    ])
+    blacklist = _to_blacklist([
+        ("pbtranscript.tasks.separate_flnc-out-0", FileTypes.PICKLE),
+        ("pbtranscript.tasks.create_chunks-out-0", FileTypes.PICKLE),
+        ("pbtranscript.tasks.create_chunks-out-1", FileTypes.PICKLE),
+        ("pbtranscript.tasks.create_chunks-out-2", FileTypes.PICKLE),
+        ("pbtranscript.tasks.combine_cluster_bins-out-1", FileTypes.JSON),
+        ("pbtranscript.tasks.combine_cluster_bins-out-7", FileTypes.PICKLE),
         ("pbtranscript.tasks.gather_ice_partial_cluster_bins_pickle-out-0",
-         FileTypes.TXT, True),
-        ("pbtranscript.tasks.cluster_bins-out-0", FileTypes.TXT, True),
-        ("pbtranscript.tasks.ice_partial_cluster_bins-out-0", FileTypes.TXT, True),
-        ("pbtranscript.tasks.ice_polish_cluster_bins-out-0", FileTypes.TXT, True),
+         FileTypes.TXT),
+        ("pbtranscript.tasks.cluster_bins-out-0", FileTypes.TXT),
+        ("pbtranscript.tasks.ice_partial_cluster_bins-out-0", FileTypes.TXT),
+        ("pbtranscript.tasks.ice_polish_cluster_bins-out-0", FileTypes.TXT),
         ("pbtranscript.tasks.gather_polished_isoforms_in_each_bin-out-0",
-         FileTypes.TXT, True),
-        ("pbtranscript.tasks.ice_cleanup-out-0", FileTypes.TXT, True),
-        ("pbreports.tasks.isoseq_cluster-out-0", FileTypes.REPORT, True)
-    ]
+         FileTypes.TXT),
+        ("pbtranscript.tasks.ice_cleanup-out-0", FileTypes.TXT),
+        ("pbreports.tasks.isoseq_cluster-out-0", FileTypes.REPORT)
+    ])
+    return _isoseq_report_rules() + _ccs_report_rules() + blacklist + whitelist
 
 
 @register_pipeline_rules("sa3_ds_isoseq", "3.2")
@@ -232,15 +318,18 @@ def isoseq_view_rules():
 @register_pipeline_rules("sa3_ds_isoseq_with_genome", "3.2")
 def isoseq_with_genome_view_rules():
     """View rules for isoseq with genome."""
-    return _isoseq_view_rules() + [
-        ("pbtranscript.tasks.map_isoforms_to_genome-out-0", FileTypes.SAM, False),
-        ("pbtranscript.tasks.post_mapping_to_genome-out-0", FileTypes.FASTQ, False),
-        ("pbtranscript.tasks.post_mapping_to_genome-out-1", FileTypes.GFF, False),
-        ("pbtranscript.tasks.post_mapping_to_genome-out-2", FileTypes.TXT, False),
-        ("pbtranscript.tasks.post_mapping_to_genome-out-3", FileTypes.TXT, False),
-        ("pbtranscript.tasks.post_mapping_to_genome-out-4", FileTypes.TXT, False),
-        ("pbtranscript.tasks.scatter_contigset_gmap-out-0", FileTypes.CHUNK, True)
-    ]
+    blacklist = _to_blacklist([
+        ("pbtranscript.tasks.scatter_contigset_gmap-out-0", FileTypes.CHUNK)
+    ])
+    whitelist = _to_whitelist([
+        ("pbtranscript.tasks.map_isoforms_to_genome-out-0", FileTypes.SAM),
+        ("pbtranscript.tasks.post_mapping_to_genome-out-0", FileTypes.FASTQ),
+        ("pbtranscript.tasks.post_mapping_to_genome-out-1", FileTypes.GFF),
+        ("pbtranscript.tasks.post_mapping_to_genome-out-2", FileTypes.TXT),
+        ("pbtranscript.tasks.post_mapping_to_genome-out-3", FileTypes.TXT),
+        ("pbtranscript.tasks.post_mapping_to_genome-out-4", FileTypes.TXT)
+    ])
+    return _isoseq_view_rules() + blacklist + whitelist
 
 
 @register_pipeline_rules("sa3_ds_isoseq_classify", "3.2")
@@ -288,14 +377,16 @@ def structural_variant_view_rules():
 
     ]
 
+
 def main(argv):
     logging.basicConfig(level=logging.INFO)
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument("--output-dir", action="store", default=os.getcwd(),
                    help="Output directory for JSON files")
     args = p.parse_args(argv[1:])
+    set_default_view_rules()
     for pipeline_id, rules in REGISTERED_VIEW_RULES.iteritems():
-        file_name = op.join(args.output_dir, "pipeline_datastore_view_rules-{p}.json".format(p=pipeline_id))
+        file_name = op.join(args.output_dir, "pipeline_datastore_view_rules-{p}.json".format(p=pipeline_id.split(".")[-1]))
         log.info("Writing {f}".format(f=file_name))
         rules.write_json(file_name)
     return 0
