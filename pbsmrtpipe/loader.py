ec2c7ed45dcae02d39a1ca95c96a41e75eb2376d
natechols <nathaniel.echols@gmail.com>
2017-08-29 07:58:16 -0700

    env var to ignore bundle for testing

--- pbsmrtpipe/loader.py
+++ pbsmrtpipe/loader.py
@@ -80,6 +80,9 @@ def _get_env_bundle_sub_dir(bundle_sub_dir):
     Get a subdirectory under the resources bundle directory configured by an
     env variable, or return None
     """
+    # workaround for running pipeline sanity checks using smrttools build
+    if ENV_IGNORE_BUNDLE in os.environ:
+        return None
     root = _get_env_path_if_defined(GlobalConstants.ENV_BUNDLE_DIR)
     if root is not None:
         return os.path.join(root, bundle_sub_dir)
