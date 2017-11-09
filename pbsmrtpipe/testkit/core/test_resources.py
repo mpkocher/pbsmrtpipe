9847a45be4b39f110d84d04dd9ff109805b5eda9
mpkocher <michael.kocher@me.com>
2017-11-09 07:22:27 -0800

    Remove "FILES" from testkit API

--- pbsmrtpipe/testkit/core/test_resources.py
+++ pbsmrtpipe/testkit/core/test_resources.py
@@ -13,12 +13,14 @@ class TestCoreResources(TestBase):
     """
     Test to see of the core job directory structure and files exist
     """
-    FILES = ('job.stdout', 'job.stderr')
+    # Directories (relative to the root job) that should be created
     DIRS = ('html', 'workflow', 'tasks', 'logs')
 
+    # Files that should exist within the 'html' subdirectory
     HTML_DIRS = ('css', 'images', 'js')
     HTML_FILES = ('index.html', 'settings.html', 'workflow.html', 'datastore.html')
 
+    # Fils that should exist within the 'workflow' directory
     WORKFLOW_FILES = ('datastore.json', 'entry-points.json', 'report-tasks.json', 'options-task.json',
                       'options-workflow.json', 'workflow.dot', 'workflow.png', 'workflow.svg')
 
